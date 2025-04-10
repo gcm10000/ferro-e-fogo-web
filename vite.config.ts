b253431e470068ae877487169425b9ffdf7f6174
gcm10000 <gabrielc.machado@hotmail.com>
2025-04-10 11:38:52 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: mode === 'production' ? '/ferro-e-fogo-web/' : '/',
+  base: mode === 'production' ? './' : '/',
   plugins: [
     react(),
     mode === 'development' &&
